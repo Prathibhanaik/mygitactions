0ec05587bb89722ff7d623bc5a0172c4354d7654
Prathibhanaik <118147361+Prathibhanaik@users.noreply.github.com>
2023-02-15 11:33:24 +0530

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Replace 'path/to/batch/file.bat' with the actual path to your batch file
+batch_file_path = 'C:/PRATHIBHA/yaml/Box.bat'
+
+# Run the batch file
+subprocess.call([batch_file_path])
