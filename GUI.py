77f00de6b4fa5f20fdd1bd07a3693dc99676dd0d
Prathibhanaik <118147361+Prathibhanaik@users.noreply.github.com>
2023-02-15 11:46:12 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Replace 'path/to/batch/file.bat' with the actual path to your batch file
-batch_file_path = 'C:/PRATHIBHA/yaml/Box.bat'
+batch_file_path = 'Box.bat'
 
 # Run the batch file
 subprocess.call([batch_file_path])
